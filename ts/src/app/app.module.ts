61f46e8307be86b9edfe4e0a2b629c9c4998b1fd
Felipe Andreas <felipe.andreas30@gmail.com>
2024-05-14 14:10:17 -0300

    Dsenvolvido o projeto mas usando meus estilos

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ts/src/app/main/main.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Assunto {
+  titulo: string;
+  autor: string;
+  conteudo: string;
+  mostrarExcluir: boolean;
+  showComments: boolean;
+
+}
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+  // Variáveis para controlar a exibição dos campos de discussão
+  mostrarCampos: boolean = false;
+  assunto: string = '';
+  conteudo: string = '';
+
+ // Variáveis para controlar a criação de novos tópicos
+  novoTopicoCriado: boolean = false;
+  assuntos: Assunto[] = [];
+  
+  // Variáveis para o novo tópico a ser criado
+  novoAssunto: string = '';
+  novoConteudo: string = '';
+  autor: string = 'Carlos Henrique Santos';
+    
+  // Implemente a função toggleExcluir
+  toggleExcluir(topico: Assunto) {
+    topico.mostrarExcluir = !topico.mostrarExcluir;
+  }
+
+  deleteTopic(topico: Assunto) {
+    // Encontre o índice do tópico no array de assuntos
+    const index = this.assuntos.indexOf(topico);
+    // Remova o tópico do array de assuntos
+    if (index !== -1) {
+        this.assuntos.splice(index, 1);
+        // Salve os tópicos atualizados no armazenamento local
+        localStorage.setItem('assuntos', JSON.stringify(this.assuntos));
+    }
+}
+
+  criarTopico() {
+    const novoAssunto: Assunto = {
+      titulo: this.assunto,
+      autor: 'Carlos Henrique Santos', // ou qualquer outra fonte de autor que você tenha
+      conteudo: this.conteudo,
+      mostrarExcluir: false,
+      showComments: false
+
+    };
+
+    // Adicionar o novo tópico ao array de tópicos
+    this.assuntos.push(novoAssunto);
+
+    // Salvar os tópicos atualizados no localStorage
+    localStorage.setItem('assuntos', JSON.stringify(this.assuntos));
+
+    // Limpar os campos de entrada após a criação do tópico
+    this.assunto = '';
+    this.conteudo = '';
+  }
+
+
+  //RESUMOS
+  textoCompleto: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet.";
+  textoResumido: string = this.textoCompleto.slice(0, 200) + "...";
+  mostrarMais: boolean = false;
+  
+    toggleMostrarMais() {
+      this.mostrarMais = !this.mostrarMais;
+    }
+
+    // COMENTARIOS
+    showComments: boolean = false;
+    commentCount: number = 1; // Inicialmente, temos um comentário
+    showComments2: boolean = false;
+    commentCount2: number = 1; // Inicialmente, temos um comentário
+
+    toggleComments(topico: Assunto) {
+      topico.showComments = !topico.showComments;
+  }
+  
+    toggleComments2() {
+      this.showComments2 = !this.showComments2;
+    }
+
+
+  constructor() {
+     // Recuperar os tópicos salvos no localStorage ao iniciar o componente
+     const assuntosSalvos = localStorage.getItem('assuntos');
+     if (assuntosSalvos) {
+         this.assuntos = JSON.parse(assuntosSalvos);
+     }
+   }
+
+  ngOnInit(): void {
+    const ultimoAssunto = localStorage.getItem('ultimoAssunto');
+    if (ultimoAssunto) {
+      this.assunto = ultimoAssunto;
+    }
+
+    // Verifique se há tópicos salvos no armazenamento local
+    const storedAssuntos = localStorage.getItem('assuntos');
+    if (storedAssuntos) {
+        this.assuntos = JSON.parse(storedAssuntos);
+    }
+  }
+
+}
